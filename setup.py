eedcc97cbcf6f449bcb5986c2382560a2b516b14
o-j-bradley <oliver.bradley11@outlook.com>
2021-08-16 15:32:18 +0100

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/o-j-bradley/testpackageojb2021',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
--- testpackageojb2021/__init__.py
+++ testpackageojb2021/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """oliver b"""
 __email__ = 'oliver.bradley@locale.app'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from testpackageojb2021.testpackageojb2021 import give_head, give_tail
