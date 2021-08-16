55b8a1fac19cde3b8f0a53cf922451b4e48ab7f4
o-j-bradley <oliver.bradley11@outlook.com>
2021-08-16 14:51:55 +0100

    finished building test

--- testpackageojb2021/testpackageojb2021.py
+++ testpackageojb2021/testpackageojb2021.py
@@ -1,1 +1,9 @@
 """Main module."""
+import pandas as pd
+
+def give_head(dataframe : pd.DataFrame):
+    return dataframe.head()
+
+if __name__ == "__main__":
+    dataframe = pd.DataFrame({"Col1": [1.0, 2.0], "Col2": [3.0, 4.0]})
+    print(give_head(dataframe= dataframe))
