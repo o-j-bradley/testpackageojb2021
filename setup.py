2fc0c5a43dc5d358ef12679f33a3c2c278f8b40e
o-j-bradley <oliver.bradley11@outlook.com>
2021-08-16 15:24:54 +0100

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/o-j-bradley/testpackageojb2021',
-    version='0.0.2',
+    version='0.0.3',
     zip_safe=False,
 )
