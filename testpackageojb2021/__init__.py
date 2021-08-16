6c6e84dcf2513037040971517fc9c0b6faea825f
o-j-bradley <oliver.bradley11@outlook.com>
2021-08-16 15:36:00 +0100

    Bump version: 0.0.4 → 0.0.5

--- testpackageojb2021/__init__.py
+++ testpackageojb2021/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """oliver b"""
 __email__ = 'oliver.bradley@locale.app'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from testpackageojb2021.testpackageojb2021 import give_head, give_tail
